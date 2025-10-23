c25ab064242be258a3b142534f3a30142c26e072
João Victor dos Santos Silva <joao.viictorss31@gmail.com>
2025-10-22 22:38:23 -0300

    chore: update dependencies and configuration, enhance styling with new CSS variables and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold">Hello world!</h1>;
+  return <Button>Button</Button>;
 }
